5ab2349dab7a0ca296d60e918b6b248826df6794
Fernando Cortés Flores <fernandocortesflores@gmail.com>
2018-11-26 15:53:37 +0100

    Set initial server listen port equals to final port

--- initial/app.js
+++ initial/app.js
@@ -4,5 +4,5 @@ var app = express();
 // Configure static server
 app.use(express.static('public'));
 
-const port = 4000;
+const port = 3000;
 app.listen(port, () =>  console.log(`Server listening ${port}`));
